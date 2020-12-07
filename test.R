eb4f85ab571039ebd398aa58148c3aad59c4d534
Ridhit Bhasin <rb20abt@herts.ac.uk>
2020-12-07 00:16:17 +0000

    First commit to plot BarGraph in my branch

--- test.R
+++ test.R
@@ -1,1 +1,11 @@
-first commit to ridhit's branch 
+#Ridhit bhasin
+#importing the csv file world happiness report 2020 into variable whr
+whr<- read.csv("World Happiness report 2020.csv")
+View(whr)
+#Simplifying the name of the columns into whr 
+names(whr)[3]<-"Ladder_score"
+names(whr)[7]<-"GDP"
+#plot Box graph between ladder score and GDP
+boxplot(whr$Ladder_score~Country.name,data = whr$GDP, xlab= "Ladder score",ylab ="GDP", main= "Ladder score(dependent) per Logged GDP per capita(Independent)",col = "orange",border = "brown",
+        horizontal = TRUE,notch = TRUE,varwidth=TRUE)
+
