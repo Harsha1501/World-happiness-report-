836c598858ce54c596641ee36b8f62d7384e5669
Ridhit Bhasin <rb20abt@herts.ac.uk>
2020-12-08 04:05:16 +0000

    Second Commit to create a bar plot graph.

--- test.R
+++ test.R
@@ -1,11 +1,39 @@
 #Ridhit bhasin
-#importing the csv file world happiness report 2020 into variable whr
-whr<- read.csv("World Happiness report 2020.csv")
-View(whr)
-#Simplifying the name of the columns into whr 
-names(whr)[3]<-"Ladder_score"
-names(whr)[7]<-"GDP"
-#plot Box graph between ladder score and GDP
-boxplot(whr$Ladder_score~Country.name,data = whr$GDP, xlab= "Ladder score",ylab ="GDP", main= "Ladder score(dependent) per Logged GDP per capita(Independent)",col = "orange",border = "brown",
-        horizontal = TRUE,notch = TRUE,varwidth=TRUE)
+library(ggplot2)
+library(dplyr)
 
+# Step1: importing the csv file world happiness report 2020 into variable whr
+whr<- read.csv("World Happiness report 2020.csv")
+#View(whr)
+# Step2: Simplifying the name of the columns into whr 
+names(whr)[1]<-"country"
+names(whr)[2]<-"region"
+names(whr)[3]<-"ladder_score"
+names(whr)[7]<-"GDP"
+
+#colnames(whr)
+# Step3 Removing unused columns: 
+whr_data <- whr %>% select(-lowerwhisker,-upperwhisker,
+-Social.support,
+-Explained.by..Log.GDP.per.capita,
+-Explained.by..Social.support,
+-Explained.by..Healthy.life.expectancy,     
+-Explained.by..Freedom.to.make.life.choices,
+-Explained.by..Generosity,                  
+-Explained.by..Perceptions.of.corruption,
+-Dystopia...residual)
+
+
+
+whr_dataset <- whr_data %>%
+  dplyr::group_by(region) %>%
+  summarise(ladder_score = mean(ladder_score),GDP = mean(GDP))
+
+
+#geom_boxplot()
+#plot Box graph between ladder score and GDP
+whr_dataset$region <- factor(whr_dataset$region,
+                             levels = rev(whr_dataset$region[order(-whr_dataset$ladder_score)]))
+ggplot(whr_dataset, aes(x=region, 
+                        y=GDP, fill=region)) + geom_bar(stat="identity") + labs(title = "Ladder_score Vs GDP for year 2020", subtitle = "") + theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 90, hjust=1))
+#boxplot(whr$Ladder_score~Country.name,data = whr$GDP, xlab= "Ladder score",ylab ="GDP", main= "Ladder score(dependent) per Logged GDP per capita(Independent)",col = "orange",border = "brown",horizontal = TRUE,notch = TRUE,varwidth=TRUE)
