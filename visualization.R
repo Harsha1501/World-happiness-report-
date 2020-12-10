a15052dc3bba50d27c4bf8a7393cab500e31db1e
Ridhit Bhasin <rb20abt@herts.ac.uk>
2020-12-10 23:11:29 +0000

    Made few changes in code

--- visualization.R
+++ visualization.R
@@ -1,13 +1,13 @@
 
 #Scatter Plot
 
-
+pdf("visualization.pdf")
 df <- read.csv("World Happiness Report 2020.csv")
 Logged_GDP_per_capita<-df$Logged.GDP.per.capita
 Ladder_score<- df$Ladder.score
 vrb <- lm(df$Logged.GDP.per.capita ~ df$Ladder.score, data= df)
 plot(df$Ladder.score, df$Logged.GDP.per.capita, xlab= 'Ladder Score', ylab='Logged GDP per capita', main="Correlation between Ladder score and Logged GDP per capita", col=c("green","blue")) + abline(vrb, col="red", lwd = 2)
-dev.copy(png,'scatter.png')
+#dev.copy(png,'scatter.png')
 
 
 #Bar plot
@@ -45,9 +45,9 @@ whr_dataset <- whr_data %>%
 
 #geom_barplot()
 #plot Bar graph between mean of ladder score per Region and GDP
+
 whr_dataset$region <- factor(whr_dataset$region,
                              levels = rev(whr_dataset$region[order(-whr_dataset$ladder_score)]))
-ggplot(whr_dataset, aes(ladder_score, 
-                        y=GDP, fill=region)) + geom_bar(stat="identity") + labs(title = "Mean of Ladder_score of Regions Vs GDP for year 2020", subtitle = "") + theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 90, hjust=1))
+ggplot(whr_dataset, aes(ladder_score,y=GDP, fill=region)) + geom_bar(stat="identity") + labs(title = "Mean of Ladder_score of Regions Vs GDP for year 2020", subtitle = "") + theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 90, hjust=1))
 
-
+dev.off()
