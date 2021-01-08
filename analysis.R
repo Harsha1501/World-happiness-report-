f356d873f523b90ab95e37f7439833ef1e2b33fd
Ridhit Bhasin <rb20abt@herts.ac.uk>
2021-01-08 17:14:58 +0000

    Added correlation test

--- analysis.R
+++ analysis.R
@@ -1,18 +1,40 @@
 library(ggplot2)
 library(dplyr)
 #importing libraries
+whr<- read.csv("World Happiness report 2020.csv")
+
+#Using Pearson's r method to check the correlation
+x<- as.numeric(whr$Logged.GDP.per.capita)
+y<-as.numeric(whr$Ladder.score)
+cor(x[!is.na(x)],y[!is.na(x)],method = "pearson")
+#Value is 0.7753744
+
+#To found of the slope and the intercept
+
+lm(formula = y[!is.na(x)]~x[!is.na(x)])
+ 
+# Performing the test to find out the p-value
+
+cor.test(x[!is.na(x)], y[!is.na(x)], method="pearson")
+
+
+#p-value < 2.2e-16 which is less than 0.05 so we can reject
+#null hypothesis and process towards the alternative hypothesis
+
+# ploting diffrent graphs for finding more evidence for alternative hypothesis
+
 
 #Loading Data-set into a variable called df1.
-df1 <- World_Happiness_Report_2020
+df1 <- read.csv("World Happiness report 2020.csv")
 
 #Plotting the Scatter-plot using the appropriate columns as per our Research Question 
-vrb <- lm(df1$`Logged GDP per capita` ~ df1$`Ladder score`, data=df1)
-plot(df1$`Ladder score`, df1$`Logged GDP per capita`, xlab = 'Ladder Score', ylab='Logged GDP per capita', main="Correlation between Ladder score and Logged GDP per capita", col=c("green","blue")) + abline(vrb, col="red", lwd = 2)
+vrb <- lm(df1$Logged.GDP.per.capita ~ df1$Ladder.score, data=df1)
+plot(df1$Ladder.score , df1$Logged.GDP.per.capita , xlab = 'Ladder Score', ylab='Logged GDP per capita', main="Correlation between Ladder score and Logged GDP per capita", col=c("green","blue")) + abline(vrb, col="red", lwd = 2)
 dev.copy(png,'plot1.png') #Saving the plot
 
 #Correlation-Coefficient for above plot 
-x<-df1$`Ladder score`
-y<-df1$`Logged GDP per capita`
+x<-df1$Ladder.score
+y<-df1$Logged.GDP.per.capita
 
 cor_coef <- cor.test(x[!is.na(x)],y[!is.na(y)], method = c("pearson"))
 print(cor_coef)
